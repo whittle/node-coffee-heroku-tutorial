cc5158d5dbede2d368a043dbb5fd7616d7206cfd
Jason Whittle <jason.whittle@gmail.com>
2011-10-19 14:02:43 -0400

    Get the request handler to end responses.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 (function() {
   var http = require('http');
 
-  var say_nothing = function() {};
+  var say_nothing = function(request, response) {
+    response.end();
+  };
 
   var app = http.createServer(say_nothing);
   app.listen(3080);
