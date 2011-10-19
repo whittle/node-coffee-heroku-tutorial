93ffcbff2abaa009fa281a02e0e18668d79e8537
Jason Whittle <jason.whittle@gmail.com>
2011-10-19 14:49:53 -0400

    Change the method for setting the content-type header from writeHead() to setHeader().

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   var http = require('http');
 
   var say_nothing = function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.setHeader('Content-Type', 'text/plain');
     response.end();
   };
 
