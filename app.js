7bab5c62f53857fbfd6487e77eaaaf906c7c04e0
Jason Whittle <jason.whittle@gmail.com>
2011-10-20 19:25:44 -0400

    Say, “Hello, world!”

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 (function() {
   var http = require('http');
 
-  var say_nothing = function(request, response) {
+  var say_hello = function(request, response) {
+    var message = 'Hello, world!';
+
     response.setHeader('Content-Type', 'text/plain');
+    response.write(message);
     response.end();
   };
 
-  var app = http.createServer(say_nothing);
+  var app = http.createServer(say_hello);
   app.listen(3080);
 })();
